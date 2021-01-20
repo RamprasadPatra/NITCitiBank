233eafdf6fd746bac4623d298e9748b46b10700f
DEVTL <DEVTL@gmail.com>
2021-01-20 11:36:15 +0530

    Created Test.java add

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -1,6 +1,12 @@
 package com.nt.beans;
+import java.util.Date;
 
 public class Utility {
+	
+	public Date getSysDate()
+	{
+		return new Date();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
