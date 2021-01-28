a0a42ec65ab474d4feb27f7f9d81c415414c77c6
DEV2 <DEV2@gmail.com>
2021-01-28 10:57:26 +0530

    M2 method

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -12,6 +12,10 @@ public class Utility {
 		System.out.println("Hello M1 method1234");
 		System.out.println("Hello M1 method1234");
 	}
+	public void M2()
+	{
+		System.out.println("Hello World!!!");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Utility u=new Utility();
